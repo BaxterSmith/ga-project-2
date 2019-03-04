5cec515da43e412e01b34cc68ac66a2668da43d0
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-04 10:42:12 -0500

    server

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
+const routes = require('./routes/index.js');
+
+app.use(methodOverride('_method'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'hbs');
+
+app.use('/', routes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`);
+});
