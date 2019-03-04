d3b2267b23967e789436c9bb51ad69ea77f77821
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-04 12:50:29 -0500

    routes for album controller

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const albumController = require('../controllers/albumController.js');
 
+router.get('/', albumController.index);
+router.get('/new', albumController.new);
+router.post('/', albumController.create);
+router.get('/:albumId', albumController.show);
+router.get('/:albumId/edit', albumController.edit);
+router.put('/:albumId', albumController.update);
+router.delete('/:albumId', albumController.delete);
+
+module.exports = router;
