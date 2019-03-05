eb549b9e73fab266dbf8c68103b8c3bd47fad608
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-05 15:21:55 -0500

    heroku

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/vinyl_db').then(() => {
-    console.log("MongoDB is connected!");
-})
+if (process.env.MONGODB_URI) {
+    mongoose.connect(process.env.MONGODB_URI);
+  }
+  else {
+    mongoose.connect('mongodb://localhost/vinyl_db');
+  }
+  mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+    }
+  );
+  mongoose.connection.once('open', function() {
+    console.log("Mongoose has connected to MongoDB!");
+  });
 
 module.exports = mongoose;
