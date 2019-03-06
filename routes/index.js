a5fe79074b2e43e8178e8da6d513c2fd852a5129
BaxterSmith <45338134+BaxterSmith@users.noreply.github.com>
2019-03-06 15:11:36 -0500

    artists and reviews

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,39 @@ const express = require('express');
 const router = express.Router();
 const albumController = require('../controllers/albumController.js');
 
+// Album routes
 router.get('/', albumController.index);
+
 router.get('/new', albumController.new);
+
 router.post('/', albumController.create);
+
 router.get('/:albumId', albumController.show);
+
 router.get('/:albumId/edit', albumController.edit);
+
 router.put('/:albumId', albumController.update);
+
 router.delete('/:albumId', albumController.delete);
+
+// Artist routes
+router.get('/artists', artistController.index);
+
+router.get('/artists/new', artistController.new);
+
+router.post('/artists', artistController.create);
+
+router.get('/artists/:artistId', artistController.show);
+
+router.delete('/artists/:artistId', artistController.delete);
+
+// Review routes
+router.get('/:albumId', reviewController.index);
+
+router.get('/:albumId/reviews/new', reviewController.new);
+
+router.post('/:albumId', reviewController.create);
+
+router.delete('/:albumId/reviews/:reviewId', reviewController.delete);
 
 module.exports = router;
